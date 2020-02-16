e9b847eecc102f20e56be7da4ff298a451d55fc8
DrBumm <53398996+DrBumm@users.noreply.github.com>
2020-02-16 10:04:04 +0100

    Update mnlp.py

--- src/mnlp.py
+++ src/mnlp.py
@@ -90,7 +90,7 @@ def start(lvl, ki=False, lives=6, delay=0.2):
                 break
     else:
         try:
-            mbki.main(lvl, lives, mlives)
+            mbki.main(lvl, lives)
         except KeyboardInterrupt:
             print("Exiting ...")
             exit()
