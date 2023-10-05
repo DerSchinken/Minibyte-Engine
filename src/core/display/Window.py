2868822c3a7258fb03dec6ace1221d60393eb17c
Paul Hippler <paulhippler21@gmail.com>
2023-10-05 12:58:26 +0200

    It's now possible to change the icon of the window

--- src/core/display/Window.py
+++ src/core/display/Window.py
@@ -1,4 +1,5 @@
-from tkinter import Tk
+from tkinter import Tk, PhotoImage, Label
+from time import time
 
 
 class Window(Tk):
@@ -6,3 +7,16 @@ class Window(Tk):
         super().__init__(*args, **kwargs)
 
         self.title("Minibyte-Engine Default Title")
+
+    def set_icon(self, icon_path: str):
+        """
+        Sets the icon of the window, will not update when icon file changes
+        Supported formats: png, jpg, ico, gif (On macOS only ico is supported)
+        :param icon_path: Path to the icon bitmap file
+        :return:
+        """
+        if not icon_path.endswith(".ico"):
+            img = PhotoImage(icon_path)
+            self.wm_iconphoto(True, img)
+        else:
+            self.wm_iconbitmap(icon_path, True)
