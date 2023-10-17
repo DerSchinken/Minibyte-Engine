ed10b3e352d68ec2ec840be6e7f29a8aef2fd71d
Paul Hippler <paulhippler21@gmail.com>
2023-10-17 11:57:29 +0200

    Added save/load shape functions

--- src/addons/shapes.py
+++ src/addons/shapes.py
@@ -1,6 +1,7 @@
 from math import tau, cos, sin
+from pickle import load as pickle_load, dump as pickle_dump
 
-from src.core.objects.Shape import Shape
+from src.core.objects.Shape import Shape, VERTICES
 
 
 def square() -> Shape:
@@ -32,10 +33,39 @@ def star() -> Shape:
 def circle() -> Shape:
     def get_all_circle_coords(x_center, y_center, radius, n_points):
         # Shamelessly stolen from: https://gis.stackexchange.com/a/395090
-        thetas = [i / n_points * tau for i in range(n_points)] # τ
+        thetas = [i / n_points * tau for i in range(n_points)]  # τ
         circle_coordinates = [(radius * cos(theta) + x_center, radius * sin(theta) + y_center) for theta in thetas]
         return circle_coordinates
 
     # Using the second function to generate all the pairs of coordinates.
     circle_vertices = get_all_circle_coords(x_center=0, y_center=0, radius=1, n_points=500)
     return Shape(circle_vertices, size=50, fill="black")
+
+
+def shape_to_file(shape: Shape, file: str, _type: str = "mbs"):
+    """
+    Saves Shapes to file
+    :param shape: the Shape to save
+    :param file: File the shape shall be saved to
+    :param _type: use 'mbs' (default) to save additional configs all other types will just save the vertices
+    """
+    if _type.lower() == "mbs":
+        with open(file, "wb") as f:
+            pickle_dump(shape, f)
+    else:
+        with open(file, "w") as f:
+            f.write(str(shape.original_vertices))
+
+
+def shape_from_file(file: str) -> Shape | VERTICES:
+    """
+    Reads either vertices from file or loads mbs files
+    :param file: mbs file or file that contains vertices
+    :return: Either Shape object or vertices
+    """
+    if file.lower().endswith("mbs"):  # mbs -> minibyte-shape
+        with open(file, "rb") as f:
+            return pickle_load(f)
+    else:
+        with open(file, "r") as f:
+            return eval(f.read())
