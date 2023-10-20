b0fa1e3158b0a9e655a088ed1ecfa689c3f559ab
Paul Hippler <paulhippler21@gmail.com>
2023-10-20 15:19:09 +0200

    Added support for images

--- src/core/objects/drawables/MBEImage.py
+++ src/core/objects/drawables/MBEImage.py
@@ -6,8 +6,20 @@ from src.core.objects.drawables.Drawable import Drawable
 
 
 class MBEImage(Drawable, PhotoImage):
-    def draw(self, master, position: POSITION, object_id: str):
-        pass  # TODO
+    __supported_file_types = ["jpg", "jpeg", "png", "gif"]
 
-    def update(self, master: Canvas, position: POSITION, object_id: str):
-        pass  # TODO
+    def __init__(self, file: str, *args, **kwargs):
+        if file.split(".")[-1] not in self.__supported_file_types:
+            raise TypeError(
+                f"'{file}' is of a not supported file type," 
+                f" for a list of supported types please look into the documentation!"
+            )
+        self.__file = file
+        super().__init__(file=self.__file, *args, **kwargs)
+
+    def draw(self, master: Canvas, position: POSITION, object_id: str) -> None:
+        master.create_image(position[0], position[1], image=self, tag=object_id)
+
+    def update(self, master: Canvas, position: POSITION, object_id: str) -> None:
+        master.delete(object_id)
+        master.create_image(position[0], position[1], image=self, tag=object_id)
