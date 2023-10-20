24219425098f5a92c57df74c70eae2e935af6a48
Paul Hippler <paulhippler21@gmail.com>
2023-10-20 08:25:37 +0200

    MBEImage now has correct signature of `Drawable`

--- src/core/objects/drawables/MBEImage.py
+++ src/core/objects/drawables/MBEImage.py
@@ -1,10 +1,13 @@
 from tkinter import PhotoImage
+
+from src.core.constants import POSITION
+from src.core.display.Canvas import Canvas
 from src.core.objects.drawables.Drawable import Drawable
 
 
 class MBEImage(Drawable, PhotoImage):
-    def draw(self, position, drawn, object_ids, drawable):
+    def draw(self, master, position: POSITION, object_id: str):
         pass  # TODO
 
-    def update(self, position, drawn, drawable):
+    def update(self, master: Canvas, position: POSITION, object_id: str):
         pass  # TODO
