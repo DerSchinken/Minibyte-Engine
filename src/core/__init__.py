2fa0912cff80a7f12f497bd200533e2c9a97d2bd
Paul Hippler <paulhippler21@gmail.com>
2023-10-23 13:09:27 +0200

    Implemented audio, with pydub, ffmpeg and pyaudio

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -2,3 +2,4 @@
 from .display import *
 from .input import *
 from .objects import *
+from .audio import *
---
+++ src/core/audio/Audio.py
@@ -0,0 +1,30 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+from threading import Thread
+
+
+class Audio:
+    def __init__(self, file: str, blocking: bool = False):
+        self.file_name = ".".join(file.split(".")[:-1])
+        self.file_type = file.split(".")[-1]
+        self.audio = AudioSegment.from_file(file, file.split(".")[-1])
+        self.blocking = blocking
+        self.__thread = None
+
+    def play(self) -> int:
+        if self.__thread is None or not self.__thread.is_alive():
+            self.__thread = Thread(
+                target=self.__play,
+                args=()
+            )
+            self.__thread.start()
+            return 1
+        else:
+            return -1
+
+    def __play(self) -> None:
+        play(self.audio)
+
+    def convert(self, new_file_type: str):
+        self.audio.export(f"{self.file_name}.{new_file_type}", format=new_file_type)
