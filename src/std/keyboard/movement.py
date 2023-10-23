34f6d3aaf4b74ea4001290dc3dc3609975f010ad
Paul Hippler <paulhippler21@gmail.com>
2023-10-23 13:10:40 +0200

    Made keyboard easier to access from outside itself; and added on collision event

--- src/std/keyboard/movement.py
+++ src/std/keyboard/movement.py
@@ -1,10 +1,12 @@
+from typing import Callable
+
 from src.core.objects.Object import Object
 from src.core.display.Window import Window
 from src.core.input.Keyboard import Keyboard
 
 
 class Movement:
-    __keyboard: Keyboard = None
+    keyboard: Keyboard = None
 
     def __init__(
             self, obj: Object, speed: int = 10, collision: bool = True,
@@ -17,7 +19,7 @@ class Movement:
         Limited to 1 global keyboard which registers events for the absolute root window!
         ^ Won't fix in the foreseeable future!
         """
-        self.__object = obj
+        self.object = obj
         self.speed = speed
         self.collision = collision
 
@@ -30,40 +32,58 @@ class Movement:
             movement_keys = {"left": ["a"], "right": ["d"], "up": ["w"], "down": ["s"]}
         self.movement_keys = movement_keys
 
-        if not Movement.__keyboard:
-            Movement.__keyboard = Keyboard(Window.ROOT)
+        if not Movement.keyboard:
+            Movement.keyboard = Keyboard(Window.ROOT)
+
+        self.__on_collision = []
 
         self.add_movement()
 
     def add_movement(self) -> None:
         for left_key in self.movement_keys["left"]:
-            self.__keyboard.on_key_down(left_key, self.move_left)
+            self.keyboard.on_key_down(left_key, self.move_left)
         for right_key in self.movement_keys["right"]:
-            self.__keyboard.on_key_down(right_key, self.move_right)
+            self.keyboard.on_key_down(right_key, self.move_right)
         for up_key in self.movement_keys["up"]:
-            self.__keyboard.on_key_down(up_key, self.move_up)
+            self.keyboard.on_key_down(up_key, self.move_up)
         for down_key in self.movement_keys["down"]:
-            self.__keyboard.on_key_down(down_key, self.move_down)
+            self.keyboard.on_key_down(down_key, self.move_down)
 
     def move_left(self) -> None:
-        if ("left" not in self.__object.check_collision() or not self.collision) and self.movable:
-            self.__object.position = (self.__object.position[0] - self.speed, self.__object.position[1])
-            self.__object.update()
+        if not self.check_collision("left") and self.movable:
+            self.object.position = (self.object.position[0] - self.speed, self.object.position[1])
+            self.object.update()
 
     def move_right(self) -> None:
-        if ("right" not in self.__object.check_collision() or not self.collision) and self.movable:
-            self.__object.position = (self.__object.position[0] + self.speed, self.__object.position[1])
-            self.__object.update()
+        if not self.check_collision("right") and self.movable:
+            self.object.position = (self.object.position[0] + self.speed, self.object.position[1])
+            self.object.update()
 
     def move_up(self) -> None:
-        if ("up" not in self.__object.check_collision() or not self.collision) and self.movable:
-            self.__object.position = (self.__object.position[0], self.__object.position[1] - self.speed)
-            self.__object.update()
+        if not self.check_collision("up") and self.movable:
+            self.object.position = (self.object.position[0], self.object.position[1] - self.speed)
+            self.object.update()
 
     def move_down(self) -> None:
-        if ("down" not in self.__object.check_collision() or not self.collision) and self.movable:
-            self.__object.position = (self.__object.position[0], self.__object.position[1] + self.speed)
-            self.__object.update()
+        if not self.check_collision("down") and self.movable:
+            self.object.position = (self.object.position[0], self.object.position[1] + self.speed)
+            self.object.update()
+
+    def check_collision(self, direction: str = None):
+        if not self.collision:
+            return False
+
+        collisions = self.object.check_collision()
+        if collisions and self.__on_collision:
+            for func in self.__on_collision:
+                try:
+                    func(self)
+                except TypeError:
+                    func()
+
+        if direction:
+            return direction in collisions
+        return collisions
 
     def disable_movement(self) -> None:
         self.movable = False
@@ -73,10 +93,19 @@ class Movement:
 
     def remove_movement(self) -> None:
         for left_key in self.movement_keys["left"]:
-            self.__keyboard.del_key_down(left_key, self.move_left)
+            self.keyboard.del_key_down(left_key, self.move_left)
         for right_key in self.movement_keys["right"]:
-            self.__keyboard.del_key_down(right_key, self.move_right)
+            self.keyboard.del_key_down(right_key, self.move_right)
         for up_key in self.movement_keys["up"]:
-            self.__keyboard.del_key_down(up_key, self.move_up)
+            self.keyboard.del_key_down(up_key, self.move_up)
         for down_key in self.movement_keys["down"]:
-            self.__keyboard.del_key_down(down_key, self.move_down)
+            self.keyboard.del_key_down(down_key, self.move_down)
+
+    def on_collision(self, func: Callable):
+        self.__on_collision.append(func)
+
+    def del_on_collision(self, func: Callable = None):
+        if func is None:
+            self.__on_collision = []
+        else:
+            self.__on_collision.remove(func)
